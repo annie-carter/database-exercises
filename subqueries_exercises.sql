892a5ec932720a1d3534a6c65f9176cf86da3ff6
annie-carter <annie.carter831@gmail.com>
2023-05-06 21:26:36 -0500

    #updated subqueries_exercises.sql, finished still need to finish  bonus.

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -23,7 +23,7 @@ WHERE first_name = 'Aamod' /*  GARBAGE; REDUNDANT = (
     AND t.to_date >NOW()
     ORDER BY t.title ASC;
     
-    -- 3
+-- 3
     -- SELECT CONCAT(first_name,' ',last_name) AS 'No Longer Working', COUNT(e.emp_no)
 SELECT COUNT(*) AS 'No Longer Working'
 FROM (
@@ -34,40 +34,44 @@ FROM (
 	) squery; -- anytime subquery in from MUST alias
     -- answer is 59900 
 
--- 4 
-SELECT *
-FROM employees
-WHERE emp_no = (
-	SELECT emp_no
-    FROM dept_manager
-	WHERE to_date > NOW（）
-    );
-    CONCAT(first_name,' ',last_name) AS 'Current Department Manager', gender
-FROM (
-	SELECT emp_no, first_
-WHERE emp_no 
-;
+-- 4
+SELECT CONCAT(e.first_name,' ',e.last_name) AS 'Current Female Managers'
+FROM employees e
+WHERE e.gender = 'F'
+		AND  e.emp_no IN (SELECT dm.emp_no
+						FROM dept_manager AS dm
+                        WHERE dm.to_date > NOW())
+ORDER BY e.first_name;
+						
+
 
 -- 5
-SELECT emp_no, salary
-FROM salaries s
-JOIN employees 
+SELECT CONCAT(e.first_name,' ',e.last_name) AS 'Current Emp Above AVG Salary', s.salary
+FROM employees e 
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN salaries s ON de.emp_no = s.emp_no
+WHERE s.to_date > NOW()
+	AND s.salary > (SELECT AVG(salary)
+					FROM salaries s
+					WHERE s.to_date < NOW())
+			ORDER BY s.salary ASC;
+            
+
 
 
 
 -- 6 
-SELECT STDDEV(salary)
-FROM salaries
-WHERE to_date> now();
+/* SELECT COUNT(*) -- CONCAT(e.first_name,' ',e.last_name) AS 'Current Emp Above AVG Salary', s.salary
+FROM salaries s 
+WHERE s.to_date > NOW()
+	AND s.salary > ((SELECT MAX(salary) FROM salaries s WHERE s.to_date < NOW()) -
+	(SELECT STD(salary) FROM salaries WHERE to_date > now)) as salary_STD;*/
+            
+      SELECT COUNT(*)
+FROM salaries s 
+WHERE s.to_date > NOW()
+	AND s.salary > ((SELECT MAX(salary) FROM salaries s WHERE s.to_date > NOW()) -
+	(SELECT STD(salary) FROM salaries WHERE to_date > NOW()));      
 
 
-SELECT count(*)
-FROM salaries
-WHERE to_date> now()
-AND salary
-SELECT MAX(salary) FROM salaries WHERE to_date > now()) -
-(SELECT STD(salary) FROM salaries WHERE to_date> now())))
-(SELECT count (*)
-FROM salaries
-WHERE to_date > now())) * 100 AS '% of salaries with STD';
-        
+
